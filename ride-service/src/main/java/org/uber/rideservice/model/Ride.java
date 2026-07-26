57b8ddab9e87346e03562c087589069c5218054d
SinhaWiz <sinhartx2070@gmail.com>
2026-07-26 15:55:47 +0600

    Ride model created

---
+++ ride-service/src/main/java/org/uber/rideservice/model/Ride.java
@@ -0,0 +1,54 @@
+package org.uber.rideservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "rides")
+public class Ride {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String riderId;
+
+    @Indexed
+    private String driverId;
+
+    private String pickupLocation;
+
+    private String dropoffLocation;
+
+    @Builder.Default
+    private RideStatus status = RideStatus.REQUESTED;
+
+    @Builder.Default
+    private Double fareEstimate = 0.0;
+
+    private Double finalFare;
+
+    @CreatedDate
+    private LocalDateTime requestedAt;
+
+    private LocalDateTime matchedAt;
+
+    private LocalDateTime startedAt;
+
+    private LocalDateTime completedAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
