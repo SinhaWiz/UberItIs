d01165b4e562fbc2a72a07de98ddb7e398061f25
SinhaWiz <sinhartx2070@gmail.com>
2026-07-26 15:58:02 +0600

    driver matching objects

---
+++ ride-service/src/main/java/org/uber/rideservice/dto/MatchDriverRequest.java
@@ -0,0 +1,13 @@
+package org.uber.rideservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MatchDriverRequest {
+
+    private String driverId;
+}
