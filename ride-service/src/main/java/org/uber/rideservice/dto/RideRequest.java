72bf817e39d8e1f24e48b5c07409767bb9f69071
SinhaWiz <sinhartx2070@gmail.com>
2026-07-26 15:57:48 +0600

    separate ride request and ride response data objects

---
+++ ride-service/src/main/java/org/uber/rideservice/dto/RideRequest.java
@@ -0,0 +1,17 @@
+package org.uber.rideservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RideRequest {
+
+    private String riderId;
+    private String pickupLocation;
+    private String dropoffLocation;
+    private Double pickupLat;
+    private Double pickupLng;
+}
---
+++ ride-service/src/main/java/org/uber/rideservice/dto/RideResponse.java
@@ -0,0 +1,29 @@
+package org.uber.rideservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.uber.rideservice.model.RideStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RideResponse {
+
+    private String id;
+    private String riderId;
+    private String driverId;
+    private String pickupLocation;
+    private String dropoffLocation;
+    private RideStatus status;
+    private Double fareEstimate;
+    private Double finalFare;
+    private LocalDateTime requestedAt;
+    private LocalDateTime matchedAt;
+    private LocalDateTime startedAt;
+    private LocalDateTime completedAt;
+}
