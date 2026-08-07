9d89cba3dab1e9fb51638d10b0c4f7ce2eeae7f6
SinhaWiz <sinhartx2070@gmail.com>
2026-08-07 21:02:19 +0600

    Add Notification entity

---
+++ notification-service/src/main/java/org/uber/notificationservice/model/Notification.java
@@ -0,0 +1,36 @@
+package org.uber.notificationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String userId;
+
+    private NotificationType type;
+
+    private String message;
+
+    @Builder.Default
+    private Boolean isRead = false;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
