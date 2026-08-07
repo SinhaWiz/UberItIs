e47eed107bdec122c1c0a4654f4ec761b5c74d61
SinhaWiz <sinhartx2070@gmail.com>
2026-08-07 21:02:04 +0600

    Add Payment entity

---
+++ payment-service/src/main/java/org/uber/paymentservice/model/Payment.java
@@ -0,0 +1,45 @@
+package org.uber.paymentservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "payments")
+public class Payment {
+
+    @Id
+    private String id;
+
+    @Indexed
+    private String rideId;
+
+    @Indexed
+    private String riderId;
+
+    @Indexed
+    private String driverId;
+
+    private Double amount;
+
+    @Builder.Default
+    private PaymentStatus status = PaymentStatus.PENDING;
+
+    @Builder.Default
+    private String paymentMethod = "CASH";
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    private LocalDateTime completedAt;
+}
