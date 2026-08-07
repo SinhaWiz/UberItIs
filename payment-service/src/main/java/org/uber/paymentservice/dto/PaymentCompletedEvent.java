da034a5c6d0fa5a8cab175cb3e4b0617cbbef21a
SinhaWiz <sinhartx2070@gmail.com>
2026-08-07 21:02:04 +0600

    Add PaymentCompletedEvent DTO for RabbitMQ publishing

---
+++ payment-service/src/main/java/org/uber/paymentservice/dto/PaymentCompletedEvent.java
@@ -0,0 +1,23 @@
+package org.uber.paymentservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentCompletedEvent {
+
+    private String paymentId;
+    private String rideId;
+    private String riderId;
+    private String driverId;
+    private Double amount;
+    private String status;
+    private LocalDateTime timestamp;
+}
