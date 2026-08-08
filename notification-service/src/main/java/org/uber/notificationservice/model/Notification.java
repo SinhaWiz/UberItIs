b3e11c51b12510665c85ff008d2134d9d1bd2e3a
Tahir Zaman <tahirzaman@Tahirs-MacBook-Air.local>
2026-08-09 01:16:48 +0600

    notification-service: add Notification document model

---
+++ notification-service/src/main/java/org/uber/notificationservice/model/Notification.java
@@ -0,0 +1,34 @@
+package org.uber.notificationservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "notifications")
+public class Notification {
+
+    @Id
+    private String id;
+
+    private String userId;
+
+    private NotificationType type;
+
+    private String message;
+
+    @Builder.Default
+    private Boolean isRead = false;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
